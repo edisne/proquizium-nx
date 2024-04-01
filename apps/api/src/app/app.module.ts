5d881165c41d2687c5f67525cee0d0e593c5de77
Edis Neziric <edis_104@hotmail.com>
2024-04-01 14:10:30 +0200

    Changed database

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -13,13 +13,19 @@ import { Question } from './questions/entities/question.entity';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '5prstiju',
+      host: 'proquizium-db-do-user-16188267-0.c.db.ondigitalocean.com',
+      port: 25060,
+      username: 'doadmin',
+      password: 'AVNS_pjHQyFs0vveF5aT0dZV',
       database: 'proquizium',
       entities: [Question, Option, Condition],
       synchronize: true,
+      ssl: true,
+      extra: {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
     }),
     QuestionsModule,
   ],
